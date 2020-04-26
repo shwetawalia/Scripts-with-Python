8e9bae73078aadc77a7619e540a2d8120484047e
shwetawalia <shwetaj0331@gmail.com>
2020-04-26 17:41:48 +0530

    Task

--- Task1.py
+++ Task1.py
@@ -1,8 +1,55 @@
-def check_ten(n1,n2):
-    if n1 + n2 ==10:
-        result = True
-    else:
-        result = False
-    return result
+'''
+    Task 1
+'''
 
-print(check_ten(10,1))
+
+# def check_ten(n1,n2):
+#     if n1 + n2 ==10:
+#         result = True
+#     else:
+#         result = False
+#     return result
+
+# print(check_ten(10,1))
+
+
+'''
+    Task 2
+'''
+
+# def check_ten(n1,n2):
+#     if n1 + n2 ==10:
+#         result = True
+#     else:
+#         result = n1+n2
+#     return result
+
+# print(check_ten(10,1))
+
+'''
+    Task 3
+'''
+
+# def first_upper(mystring):
+#     print(mystring[0].upper()+mystring[1:])
+    
+
+# first_upper("shweta")
+
+
+'''
+    Task 4
+'''
+
+# def last_two(mystring):
+#     if mystring.count(mystring)>=2:
+#         print("Error")
+#     else:
+
+#         print(mystring[-2:])
+
+# last_two("shweta")
+
+'''
+    Task 5
+'''
